ab52bf105e733561920fba62859a714c4771a548
Franco <132803088+FrancoA21@users.noreply.github.com>
2023-10-13 21:54:25 -0300

    Franco

---
+++ Models/IngresosModels.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace miBilletera.Models
+{
+    public class IngresosModels
+    {
+        public int Id_Ingresos { get; set; }
+        public string Nombre { get; set; } ="";
+    }
+}
